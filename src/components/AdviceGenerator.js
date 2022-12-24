a671253c6a19e8468177611eed137782c9ec308b
die_narzisse <bulainarcis7@gmail.com>
2022-12-24 19:37:09 +0100

    text size

--- src/components/AdviceGenerator.js
+++ src/components/AdviceGenerator.js
@@ -17,7 +17,7 @@ function AdviceGenerator(){
     return (
         <div className="bg-[#313A48] flex flex-col justify-center items-center min-h-[19.75rem] w-[21.625rem] lg:w-[540px] rounded-xl relative px-[24px] py-[40px]">
             <h1 className="text-[#53FFAA] text-[11px] font-extrabold tracking-[3.46px] uppercase mb-[24px]">Advice #{adviceID}</h1>
-            <section className="text-[#CEE3E9] text-[24px] font-extrabold mb-[24px] text-center">{ advice }</section>
+            <section className="text-[#CEE3E9] lg:max-w-[444px] text-[24px] font-extrabold mb-[24px] text-center">{ advice }</section>
             <Divider />
             <Button tailwindButton={"bg-[#53FFAA] p-4 rounded-full absolute bottom-[-32px] hover:shadow-[0_0px_40px_-5px_#53FFAA]"}
              tailwindWrapper={"flex justify-center items-center"}
